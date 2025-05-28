4c12d781bbf2183a8fb5db24bd832de01a41ff85
Hernael <hperer@movealong.tech>
2025-05-27 23:07:47 -0400

    update api

--- app/src/main/java/io/samancore/workflow/app/api/Api.java
+++ app/src/main/java/io/samancore/workflow/app/api/Api.java
@@ -1,5 +1,6 @@
 package io.samancore.workflow.app.api;
 
+import io.samancore.workflow.app.util.GraphQueries;
 import io.samancore.workflow.model.State;
 import io.samancore.workflow.model.Transition;
 import jakarta.annotation.security.RolesAllowed;
@@ -16,27 +17,27 @@ public class Api {
     @Path("{product}/state/{stateId}")
     @RolesAllowed({"admin"})
     public State getState(@PathParam("product") String product, @PathParam("stateId") String stateId) {
-        return null;
+        return GraphQueries.getVertex(product, stateId);
     }
 
     @GET
     @Path("{product}/transition/{transitionId}")
     @RolesAllowed({"admin"})
     public Transition getTransition(@PathParam("product") String product, @PathParam("transitionId") String transitionId) {
-        return null;
+        return GraphQueries.getEdge(product, transitionId);
     }
 
     @GET
     @Path("{product}/state/{stateId}/transitions")
     @RolesAllowed({"admin"})
     public List<Transition> getTransitionsByState(@PathParam("product") String product, @PathParam("stateId") String stateId) {
-        return null;
+        return GraphQueries.getEdgesFromVertex(product, stateId);
     }
 
     @GET
     @Path("{product}/initial-transitions")
     @RolesAllowed({"admin"})
     public List<Transition> getInitialTransitions(@PathParam("product") String product) {
-        return null;
+        return GraphQueries.getInitialEdges(product);
     }
 }
--- app/src/main/java/io/samancore/workflow/app/util/GraphQueries.java
+++ app/src/main/java/io/samancore/workflow/app/util/GraphQueries.java
@@ -5,8 +5,6 @@ import io.samancore.workflow.model.Transition;
 import jakarta.ws.rs.NotFoundException;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import static io.samancore.workflow.app.util.GraphCache.GRAPHS;
 
@@ -34,22 +32,12 @@ public class GraphQueries {
         return GRAPHS.get(productName).outgoingEdgesOf(state).stream().toList();
     }
 
-    public static State getTargetVertex(String productName, String edgeId) {
-        Transition transition = new Transition(edgeId, null, null, null, null, null);
-        return GRAPHS.get(productName).getEdgeTarget(transition);
-    }
-
-    public static State getSourceVertex(String productName, String edgeId) {
-        Transition transition = new Transition(edgeId, null, null, null, null, null);
-        return GRAPHS.get(productName).getEdgeSource(transition);
-    }
-
-    public static Set<State> getVerticesNotTarget(String productName) {
-        Set<State> targetVertices = GRAPHS.get(productName).edgeSet().stream()
-                .map(GRAPHS.get(productName)::getEdgeTarget)
-                .collect(Collectors.toSet());
+    public static List<Transition> getInitialEdges(String productName) {
         return GRAPHS.get(productName).vertexSet().stream()
-                .filter(vertex -> !targetVertices.contains(vertex))
-                .collect(Collectors.toSet());
+                .filter(State::isInitial)
+                .findFirst()
+                .map(state -> GRAPHS.get(productName).outgoingEdgesOf(state).stream().toList())
+                .orElseGet(List::of);
+
     }
 }
